3b50299206c585f2082560484f6dd3f0434c6c9a
vkarchevskyi <vkarchevskyi10@gmail.com>
2025-02-02 16:48:47 +0200

    install packages

---
+++ src/Services/Classroom/GoogleAuthService.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Classroom;
+
+use Google\Client;
+use Google\Exception;
+
+class GoogleOauthService
+{
+    /**
+     * @throws Exception
+     */
+    public function run(): void
+    {
+    }
+}
